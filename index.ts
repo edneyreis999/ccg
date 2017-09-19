88ee37dfe9c9adfce451e6d6d1607504bb12b193
Edney <edney_reis999@hotmail.com>
2017-09-18 23:52:19 -0300

    fist commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import * as hapi from "hapi";
+
+const server: hapi.Server = new hapi.Server()
+server.connection({ port: 3000 });
+
+server.route({
+    method: "GET",
+    path: "/",
+    handler: (request: hapi.Request, reply: hapi.ReplyNoContinue) => {
+        reply("Hello World");
+    }
+
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("server running at 3000");
+})
